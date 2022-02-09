f700ef4307f4c71e169a70bbfc11df11e47db14f
Rando 87 <68273711+tharunyogenthra@users.noreply.github.com>
2022-02-10 01:41:57 +1100

    Fixed the line problem

--- webscraper.js
+++ webscraper.js
@@ -2,7 +2,16 @@
 let subjectArr = [];
 let subjectValue;
 for (let i = 0; i < 6; i++) {
-    subjectValue = (`${i + 1}) `+ document.getElementsByClassName("timetable-class")[i].getElementsByTagName('strong')[0].innerHTML + " / " + document.getElementsByClassName("timetable-class")[i].getElementsByTagName('strong')[1].innerHTML);subjectArr.push(subjectValue);console.log(subjectValue);
+    subjectValue =   
+        (`${i + 1}) `
+        + document.getElementsByClassName
+        ("timetable-class")[i].getElementsByTagName
+        ('strong')[0].innerHTML 
+        + " / " 
+        + document.getElementsByClassName
+        ("timetable-class")[i].getElementsByTagName
+        ('strong')[1].innerHTML);
+        subjectArr.push(subjectValue);
+        console.log(subjectValue);
 }
 console.log(subjectArr);
-
